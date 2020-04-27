fd3cc53204177232cd19f7b3030e1005953e8c6d
Jacob Partusch <jakepartusch@gmail.com>
2020-04-26 22:40:09 -0500

    feat: adding sanity

---
+++ cms/schemas/product.js
@@ -0,0 +1,67 @@
+export default {
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      title: "Price",
+      name: "price",
+      type: "number",
+    },
+    {
+      title: "SKU",
+      name: "sku",
+      type: "string",
+    },
+    {
+      name: "images",
+      title: "Images",
+      type: "array",
+      of: [
+        {
+          type: "image",
+          options: {
+            hotspot: true,
+          },
+        },
+      ],
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [
+        {
+          type: "reference",
+          to: { type: "category" },
+        },
+      ],
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "localeBlockContent",
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      media: "images[0]",
+    },
+  },
+}
